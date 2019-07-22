3fe902e76619412f542b9580b49bd37fd26cf24a
stbe0616 <ibtsni@gmail.com>
2019-07-22 19:04:58 +0400

    link to file

--- mainapp/mainapp/settings.py
+++ mainapp/mainapp/settings.py
@@ -122,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/genereted_docs"),
+    os.path.join(BASE_DIR, "static/genereted_docs/txt"),
+    os.path.join(BASE_DIR, "static/genereted_docs/pdf"),
+    os.path.join(BASE_DIR, "static/genereted_docs/exel"),
+    os.path.join(BASE_DIR, "static/genereted_docs/doc"),
+]
