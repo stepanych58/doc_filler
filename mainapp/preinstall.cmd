f3ac601cad60e2de6c0fbb425e396619f499d0c1
stepanych58 <ibtsni@yandex.ru>
2020-02-27 13:54:26 +0400

    preinstall script

---
+++ mainapp/preinstall.cmd
@@ -0,0 +1,8 @@
+pip install Django==2.2.1
+pip install django-json-field
+pip install python-dateutil six
+pip install psycopg2
+pip install jsonfield
+pip install django-allauth
+pip install pypdf2
+
