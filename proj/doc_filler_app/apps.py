9115bcc10cb20b7275eb7aa7996267d6c3215d46
saleny <52565823+saleny@users.noreply.github.com>
2020-02-27 23:50:04 +0400

    delete unnecessary

--- proj/doc_filler_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocFillerAppConfig(AppConfig):
-    name = 'doc_filler_app'
--- proj/mig_and_run.cmd
+++
@@ -1,4 +0,0 @@
-del /f db.sqlite3
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
