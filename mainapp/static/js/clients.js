69df94c5674054965d73bca44fe74a59e1d84330
saleny <52565823+saleny@users.noreply.github.com>
2020-03-25 23:13:01 +0400

    fix search in tables

--- mainapp/static/js/clients.js
+++ mainapp/static/js/clients.js
@@ -254,26 +254,6 @@ function rentalPropertyForm() {
     return;
 }
 
-function tableSearch() {
-    var phrase = document.getElementById('searchs-text');
-    var table = document.getElementById('info-table');
-    var regPhrase = new RegExp(phrase.value, 'i');
-    var flag = false;
-    console.log(phrase)
-    for (var i = 1; i < table.rows.length; i++) {
-        flag = false;
-        for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
-            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
-            if (flag) break;
-        }
-        if (flag) {
-            table.rows[i].style.display = "";
-        } else {
-            table.rows[i].style.display = "none";
-        }
-    }
-}
-
 function check(name)
 {
  var check=document.getElementsByName(name);
